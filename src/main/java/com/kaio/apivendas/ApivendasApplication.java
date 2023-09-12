4f4a7085afb4771bbe737aac14065dbba9ae154c
kaiotcp1 <kaiotcp1@gmail.com>
2023-09-12 14:31:35 -0300

    Estrutura base

---
+++ src/main/java/com/kaio/apivendas/ApivendasApplication.java
@@ -0,0 +1,13 @@
+package com.kaio.apivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApivendasApplication.class, args);
+    }
+
+}
