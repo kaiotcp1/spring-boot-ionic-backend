64983a6a980d5d8a111b8ff83b94a1c4563f3ab7
kaiotcp1 <kaiotcp1@gmail.com>
2023-09-13 15:35:42 -0300

    Ciração das Entidades Produto e Categoria com seus relacionamentos e Exceções personalizadas

---
+++ src/main/java/com/kaio/apivendas/resources/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.kaio.apivendas.resources;
+
+import com.kaio.apivendas.domain.Categoria;
+import com.kaio.apivendas.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/kaio/apivendas/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.kaio.apivendas.services;
+
+import com.kaio.apivendas.domain.Categoria;
+import com.kaio.apivendas.repositories.CategoriaRepository;
+import com.kaio.apivendas.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+}
