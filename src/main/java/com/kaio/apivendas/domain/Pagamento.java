ae00470105469b9f801a62d6d4294a7040571f0e
kaiotcp1 <kaiotcp1@gmail.com>
2023-09-21 14:51:38 -0300

    Relacionamento Bidirecional OneToOne

---
+++ src/main/java/com/kaio/apivendas/domain/Pagamento.java
@@ -0,0 +1,69 @@
+package com.kaio.apivendas.domain;
+
+import com.kaio.apivendas.domain.enums.EstadoPagamento;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Pagamento implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    private Integer id;
+    private EstadoPagamento estado;
+
+    @OneToOne
+    @JoinColumn(name = "pedido_id")
+    @MapsId
+    private Pedido pedido;
+
+    public Pagamento() {
+    }
+
+    public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+        this.id = id;
+        this.estado = estado;
+        this.pedido = pedido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public EstadoPagamento getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoPagamento estado) {
+        this.estado = estado;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pagamento pagamento = (Pagamento) o;
+        return Objects.equals(getId(), pagamento.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
