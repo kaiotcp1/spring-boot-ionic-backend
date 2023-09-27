db48e7b22e22d640d592d6921459405adb60dde1
kaiotcp1 <kaiotcp1@gmail.com>
2023-09-27 15:15:29 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/kaio/apivendas/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.kaio.apivendas.repositories;
+
+import com.kaio.apivendas.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
