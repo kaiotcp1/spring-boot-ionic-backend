b5af18e85d1dc1d5faf46780459b4c46effef1ff
kaiotcp1 <kaiotcp1@gmail.com>
2023-10-06 16:24:18 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/kaio/apivendas/resources/CategoriaResource.java
+++ src/main/java/com/kaio/apivendas/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.kaio.apivendas.resources;
 
 import com.kaio.apivendas.domain.Categoria;
+import com.kaio.apivendas.dto.CategoriaDTO;
 import com.kaio.apivendas.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -41,6 +44,13 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
