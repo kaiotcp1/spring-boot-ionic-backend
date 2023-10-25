8be252852e4cef5424301d312a0add9374717d25
kaiotcp1 <kaiotcp1@gmail.com>
2023-10-25 18:07:55 -0300

    Vlidação customizada de CPF ou CNPJ na inserção do Cliente

--- src/main/java/com/kaio/apivendas/dto/ClienteNewDTO.java
+++ src/main/java/com/kaio/apivendas/dto/ClienteNewDTO.java
@@ -1,25 +1,38 @@
 package com.kaio.apivendas.dto;
 
+import com.kaio.apivendas.services.validation.ClienteInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serial;
 import java.io.Serializable;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Email inválido")
     private String email;
     private String cpfOuCnpj;
     private Integer tipo;
 
-
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String logradouro;
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String numero;
     private String complemento;
     private String bairro;
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String cep;
 
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String telefone1;
     private String telefone2;
     private String telefone3;
